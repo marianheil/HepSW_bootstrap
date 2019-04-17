2be34ebe5a8d8950f72e3ec63647855b48edae2d
Marian Heil <marian.heil@durham.ac.uk>
2019-04-17 16:18:00 +0100

    Script to download PDFs

---
+++ LHAPDF/LHAPDF-download_PDFs.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+source ../config
+
+## package specific variables
+name=LHAPDF
+
+InstallDir=${HEPSW_HOME}/${name}-${HEPSW_LHAPDF_VERSION}
+
+## download all PDF sets
+all_pdfs=($(${InstallDir}/bin/lhapdf ls))
+installed_pdfs=($(${InstallDir}/bin/lhapdf ls --installed))
+failed_downloads=()
+for PDF in ${all_pdfs[@]}
+do
+  if (echo "${installed_pdfs[@]}"  | fgrep -wq "${PDF}")
+  then
+    echo "${PDF} already installed"
+  else
+    wget --no-check-certificate ${HEPSW_LHAPDF_PDF_SOURCE}/${PDF}.tar.gz -O- \
+      | tar xz -C ${InstallDir}/share/LHAPDF || failed_downloads+=(${PDF})
+  fi
+done
+
+if [ ${#failed_downloads[@]} -eq 0 ];
+then
+  echo "All PDF sets successfully downloaded."
+else
+  echo "Download failed for ${#failed_downloads[@]}/${#all_pdfs[@]} PDFs:"
+  echo ${failed_downloads[@]}
+fi
