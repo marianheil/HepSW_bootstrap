43b5c5349843329ec0fcf48f1865f05bbe9c81fb
Marian Heil <marian.heil@durham.ac.uk>
2019-04-17 17:03:14 +0100

    Installer HepMC2

---
+++ HepMC2/HepMC2-Installer.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+source ../config
+
+## package specific variables
+name=HepMC2
+package_name=hepmc${HEPSW_HEPMC2_VERSION}
+
+InstallDir=${HEPSW_HOME}/${name}-${HEPSW_HEPMC2_VERSION}
+
+## download
+cd ${WORKING_DIR}
+wget -O- http://hepmc.web.cern.ch/hepmc/releases/${package_name}.tgz | \
+  tar zx || exit 1
+mkdir ${name}-build
+cd ${name}-build
+
+## install
+cmake3 -DCMAKE_INSTALL_PREFIX=${InstallDir} -Dmomentum:STRING=GEV \
+      -Dlength:STRING=MM ../${package_name} || exit 2
+make -j${NUM_CORES} || exit 2
+make install || exit 3
+rm -rf ${WORKING_DIR}/${package_name}
+rm -rf ${WORKING_DIR}/${name}-build
+
+## environment
+cd ${BASE_DIR}
+cp ../TEMPLATEenv.sh ${InstallDir}/${name}env.sh
+sed -i -e "s TEMPLATE_PREFIX ${InstallDir} g" ${InstallDir}/${name}env.sh
+sed -i -e "s/TEMPLATE/${name}/g" ${InstallDir}/${name}env.sh
+sed -i -e "s/#.export.*//g" ${InstallDir}/${name}env.sh
