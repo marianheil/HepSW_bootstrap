f8bd8e8815187a8425cfc16de562653d823a7687
Marian Heil <marian.heil@durham.ac.uk>
2019-04-18 09:49:04 +0100

    Installer OpenLoops

---
+++ OpenLoops/OpenLoops-Installer.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+source ../config
+
+## package specific variables
+name=${HEPSW_OPENLOOPS_NAME}
+package_name=${name}-${HEPSW_OPENLOOPS_VERSION}
+
+InstallDir=${HEPSW_OPENLOOPS_DIR}
+
+## download
+mkdir ${InstallDir}
+cd ${InstallDir}
+git clone https://gitlab.com/openloops/OpenLoops.git . || exit 1
+
+## install
+./scons
+
+## no environment needed
+cd ${BASE_DIR}
---
+++ OpenLoops/OpenLoops-download_processes.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+source ../config
+
+## package specific variables
+InstallDir=${HEPSW_OPENLOOPS_DIR}
+# list of process to be installed
+processes=(ppjj ppjj_ew ppjjj ppjjj_ew ppjjjj \
+  heftpphj heftpphjj heftpphjjj pph2 pphbb bbhj pphbbj pphj2 pphjj2 pphjjj2 \
+  pphjj_vbf pphjj_vbf_ew \
+  ppaj2 ppajj ppajj2 ppajj_ew ppajjj \
+  ppll2 ppllj ppllj2 ppllj_ew ppllj_nf5 pplljj pplljj_ew pplljjj \
+  pplnj_ckm pplnjj pplnjj_ckm pplnjj_ew pplnjjj)
+
+## install all processes
+cd ${InstallDir}
+for process in ${processes[@]}
+do
+  echo "Installing" ${process}
+  ./openloops libinstall ${process} || exit 1
+done
+# rm -r process_src/* process_obj/* || exit 2
