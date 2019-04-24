0d8f39ac27f6a5dbd1f199de9abe9ffc97a80c40
Marian Heil <marian.heil@durham.ac.uk>
2019-04-24 17:14:29 +0100

    Installer boost

---
+++ boost/boost-Installer.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+source ../config
+
+## package specific variables
+name=${HEPSW_BOOST_NAME}
+package_name=${name}_$(echo ${HEPSW_BOOST_VERSION} | sed -e "s/\./_/g")
+
+InstallDir=${HEPSW_BOOST_DIR}
+
+## download
+cd ${WORKING_DIR}
+mkdir ${package_name}
+wget -O- \
+  https://dl.bintray.com/boostorg/release/${HEPSW_BOOST_VERSION}/source/${package_name}.tar.gz | \
+  tar zx || exit 1
+cd ${package_name}
+./bootstrap.sh --prefix=${InstallDir} --with-python=$(which python) \
+  --with-libraries=iostreams,python || exit 2
+
+## install
+./b2 -j${NUM_CORES} install --layout=tagged || exit 3
+rm -rf ${WORKING_DIR}/${package_name}
+
+## environment
+cd ${BASE_DIR}
+cp ../TEMPLATEenv.sh ${InstallDir}/${name}env.sh
+sed -i -e "s TEMPLATE_PREFIX ${InstallDir} g" ${InstallDir}/${name}env.sh
+sed -i -e "s/TEMPLATE/${name}/g" ${InstallDir}/${name}env.sh
+sed -i -e "s/#.export.PATH/export PATH/g" ${InstallDir}/${name}env.sh
+sed -i -e "s/#.export.*//g" ${InstallDir}/${name}env.sh
