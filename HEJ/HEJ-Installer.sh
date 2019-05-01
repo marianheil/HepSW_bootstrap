1330e580815066ca7ad3db1595f9af14a88800a9
Marian Heil <marian.heil@durham.ac.uk>
2019-05-01 11:54:56 +0100

    Added yaml dependence to HEJ

--- HEJ/HEJ-Installer.sh
+++ HEJ/HEJ-Installer.sh
@@ -9,43 +9,8 @@ package_name=${name}-${HEPSW_HEJ_VERSION}
 InstallDir=${HEPSW_HEJ_DIR}
 git_branch=v$(echo ${HEPSW_HEJ_VERSION} | cut -f1,2 -d ".")
 
-## download
-cd ${WORKING_DIR}
-git clone -b ${git_branch} https://phab.hepforge.org/source/hej.git ${package_name} || exit 1
-cd ${package_name}
-git reset --hard ${HEPSW_HEJ_VERSION}
-
-## install HEJ
-mkdir build
-cd build
-cmake3 -DCMAKE_INSTALL_PREFIX=${InstallDir} -DBOOST_ROOT=${HEPSW_BOOST_DIR} \
-  -DHepMC_ROOT_DIR=${HEPSW_HEPMC2_DIR} .. || exit 2
-make -j${NUM_CORES} || exit 2
-make install || exit 3
-
-## environment
-cd ${BASE_DIR}
-cp ../TEMPLATEenv.sh ${InstallDir}/${name}env.sh
-sed -i -e "s TEMPLATE_PREFIX ${InstallDir} g" ${InstallDir}/${name}env.sh
-sed -i -e "s/TEMPLATE/${name}/g" ${InstallDir}/${name}env.sh
-sed -i -e "s/#.export.PATH/export PATH/g" ${InstallDir}/${name}env.sh
-sed -i -e "s/#.export.*//g" ${InstallDir}/${name}env.sh
-
-## install HEJFOG
-source ${InstallDir}/${name}env.sh
-cd ${WORKING_DIR}/${package_name}/FixedOrderGen
-mkdir build
-cd build
-cmake3 -DCMAKE_INSTALL_PREFIX=${InstallDir} -DBOOST_ROOT=${HEPSW_BOOST_DIR} \
-  .. || exit 2
-echo 0
-make -j${NUM_CORES} || exit 2
-make install || exit 3
-
-rm -rf ${WORKING_DIR}/${package_name}
-
-## save dependeces
-cd ${BASE_DIR}
+## dependences
+mkdir -p ${InstallDir}
 # boost
 printf "## ${HEPSW_BOOST_NAME} ${HEPSW_BOOST_VERSION}\n" \
   > ${InstallDir}/${name}dependeces.sh
@@ -81,3 +46,46 @@ printf "## ${HEPSW_RIVET_NAME} ${HEPSW_RIVET_VERSION}\n" \
   >> ${InstallDir}/${name}dependeces.sh
 printf "source ${HEPSW_RIVET_DIR}/${HEPSW_RIVET_NAME}env.sh\n" \
   >> ${InstallDir}/${name}dependeces.sh
+# yaml-cpp
+printf "## ${HEPSW_YAML_NAME} ${HEPSW_YAML_VERSION}\n" \
+  >> ${InstallDir}/${name}dependeces.sh
+printf "source ${HEPSW_YAML_DIR}/${HEPSW_YAML_NAME}env.sh\n" \
+  >> ${InstallDir}/${name}dependeces.sh
+
+source ${InstallDir}/${name}dependeces.sh || exit 1
+
+## download
+cd ${WORKING_DIR}
+git clone -b ${git_branch} https://phab.hepforge.org/source/hej.git ${package_name} || exit 1
+cd ${package_name}
+git reset --hard ${HEPSW_HEJ_VERSION}
+
+## install HEJ
+mkdir build
+cd build
+cmake3 -DCMAKE_INSTALL_PREFIX=${InstallDir} -DBOOST_ROOT=${HEPSW_BOOST_DIR} \
+  -DHepMC_ROOT_DIR=${HEPSW_HEPMC2_DIR} .. || exit 2
+make -j${NUM_CORES} || exit 2
+make install || exit 3
+
+## environment
+cd ${BASE_DIR}
+cp ../TEMPLATEenv.sh ${InstallDir}/${name}env.sh
+sed -i -e "s TEMPLATE_PREFIX ${InstallDir} g" ${InstallDir}/${name}env.sh
+sed -i -e "s/TEMPLATE/${name}/g" ${InstallDir}/${name}env.sh
+sed -i -e "s/#.export.PATH/export PATH/g" ${InstallDir}/${name}env.sh
+sed -i -e "s/#.export.*//g" ${InstallDir}/${name}env.sh
+
+## install HEJFOG
+source ${InstallDir}/${name}env.sh
+cd ${WORKING_DIR}/${package_name}/FixedOrderGen
+mkdir build
+cd build
+cmake3 -DCMAKE_INSTALL_PREFIX=${InstallDir} -DBOOST_ROOT=${HEPSW_BOOST_DIR} \
+  .. || exit 2
+make -j${NUM_CORES} || exit 2
+make install || exit 3
+
+## Cleanup
+cd ${BASE_DIR}
+rm -rf ${WORKING_DIR}/${package_name}
