83f5005512fd6187128756e2eb18627067699a30
Marian Heil <marian.heil@durham.ac.uk>
2019-09-19 16:50:49 +0100

    updated HepMC3 -> 3.1.2 (now included root IO)

--- HepMC3/HepMC3-Installer.sh
+++ HepMC3/HepMC3-Installer.sh
@@ -9,19 +9,29 @@ package_name=${name}-${HEPSW_HEPMC3_VERSION}
 
 InstallDir=${HEPSW_HEPMC3_DIR}
 
+## dependences
+mkdir -p ${InstallDir}
+# root
+printf "## ${HEPSW_ROOT_NAME} ${HEPSW_ROOT_VERSION}\n" \
+  >> ${InstallDir}/${name}dependences.sh
+printf "source ${HEPSW_ROOT_DIR}/${HEPSW_ROOT_NAME}env.sh\n" \
+  >> ${InstallDir}/${name}dependences.sh
+
+source ${InstallDir}/${name}dependences.sh || exit 1
+
 ## download
 cd ${WORKING_DIR}
 # wget -O- http://hepmc.web.cern.ch/hepmc/releases/${package_name}.tgz | \ # old style < 3.1
 wget -O- http://hepmc.web.cern.ch/hepmc/releases/${package_name}.tar.gz | \
-  tar xz || exit 1
+  tar xz || exit 2
 cd ${package_name}
 mkdir build
 cd build
 
 ## install
-cmake3 -DCMAKE_INSTALL_PREFIX=${InstallDir} .. || exit 2
-make -j${NUM_CORES} || exit 2
-make install || exit 3
+cmake3 -DCMAKE_INSTALL_PREFIX=${InstallDir} .. || exit 3
+make -j${NUM_CORES} || exit 3
+make install || exit 4
 rm -rf ${WORKING_DIR}/${package_name}
 
 ## environment
