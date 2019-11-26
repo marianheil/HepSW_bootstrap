cbff090ca2bc8a8508d3aae52a3b6574ad3792ed
Marian Heil <marian.heil@durham.ac.uk>
2019-11-26 20:04:49 +0100

    Updated rivet to version 3
    now uses HepMC 3 & not the rivet bootstrap

--- rivet/Rivet-Installer.sh
+++ rivet/Rivet-Installer.sh
@@ -5,7 +5,7 @@ source ../config
 
 ## package specific variables
 name=${HEPSW_RIVET_NAME}
-package_name=${HEPSW_RIVET_VERSION}
+package_name=${name^}-${HEPSW_RIVET_VERSION}
 dependencies=${HEPSW_RIVET_DEPENDENCIES[@]}
 
 InstallDir=${HEPSW_RIVET_DIR}
@@ -27,16 +27,16 @@ source ${InstallDir}/${name}dependencies.sh || exit 1
 
 ## download
 cd ${WORKING_DIR}
-mkdir ${package_name}
+wget -O- https://rivet.hepforge.org/downloads/?f=${package_name}.tar.gz | tar xz
 cd ${package_name}
-wget -O rivet-bootstrap \
-  https://phab.hepforge.org/source/rivetbootstraphg/browse/${package_name}/rivet-bootstrap?view=raw
-chmod +x rivet-bootstrap
+
+./configure --prefix=${InstallDir} \
+  --with-yoda=${HEPSW_YODA_DIR} \
+  --with-fastjet=${HEPSW_FASTJET_DIR} \
+  --with-hepmc3=${HEPSW_HEPMC3_DIR} || exit 2
 
 ## install
-INSTALL_FASTJET=0 FASTJETPATH=${HEPSW_FASTJET_DIR} \
-  INSTALL_HEPMC=0 HEPMCPATH=${HEPSW_HEPMC2_DIR} \
-  INSTALL_PREFIX=${InstallDir} MAKE="make -j ${NUM_CORES}" \
-  ./rivet-bootstrap
+make -j${NUM_CORES} || exit 3
+make install || exit 3
 
 rm -rf ${WORKING_DIR}/${package_name}
