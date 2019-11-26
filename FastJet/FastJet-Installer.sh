3417d25c287abf13ce8c0deca5508b501e7ee87e
Marian Heil <marian.heil@durham.ac.uk>
2019-11-26 18:57:40 +0100

    Added fastjet contrib

--- FastJet/FastJet-Installer.sh
+++ FastJet/FastJet-Installer.sh
@@ -45,3 +45,15 @@ sed -i -e "s/TEMPLATE/${name}/g" ${InstallDir}/${name}env.sh
 sed -i -e "s/#.export.PATH/export PATH/g" ${InstallDir}/${name}env.sh
 sed -i -e "s/#.export.*//g" ${InstallDir}/${name}env.sh
 printf 'export PYTHONPATH='$InstallDir'/lib/python2.7/site-packages:'$InstallDir'/lib64/python2.7/site-packages:${PYTHONPATH}\n' >> ${InstallDir}/${name}env.sh
+
+## install FastJet Contrib
+source ${InstallDir}/${name}env.sh
+cd ${WORKING_DIR}
+wget -O- http://fastjet.hepforge.org/contrib/downloads/fjcontrib-${HEPSW_FASTJET_CONTRIB_VERSION}.tar.gz \
+  | tar zx || exit 4
+cd fjcontrib-${HEPSW_FASTJET_CONTRIB_VERSION}
+./configure || exit 5
+make -j${NUM_CORES} || exit 5
+make check || exit 5
+make install || exit 5
+rm -rf ${WORKING_DIR}/fjcontrib
