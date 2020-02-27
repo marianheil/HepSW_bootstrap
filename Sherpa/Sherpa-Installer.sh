55b8d144d2d86ca0c27965414f52435c4dd419dd
Marian Heil <marian.heil@durham.ac.uk>
2020-02-27 12:18:51 +0000

    Move common initialisation block to one file

--- Sherpa/Sherpa-Installer.sh
+++ Sherpa/Sherpa-Installer.sh
@@ -1,33 +1,20 @@
 #!/usr/bin/env bash
 
 source ../config
-../init.sh
-# overwrite compiler for MPI
-export CC=${MPICXX}   # C compiler
-export CXX=${MPICXX} # C++ compiler
 
 ## package specific variables
 name=${HEPSW_SHERPA_NAME}
 package_name=${name}-${HEPSW_SHERPA_VERSION}
 dependencies=${HEPSW_SHERPA_DEPENDENCIES[@]}
-
 InstallDir=${HEPSW_SHERPA_DIR}
 git_branch=rel-$(echo ${HEPSW_SHERPA_VERSION} | sed -e "s/\./-/g")
 
-## dependencies
-mkdir -p ${InstallDir}
-printf "" > ${InstallDir}/${name}dependencies.sh
-for dep in ${dependencies[@]}; do
-  dep_name="HEPSW_${dep}_NAME"
-  dep_version="HEPSW_${dep}_VERSION"
-  dep_dir="HEPSW_${dep}_DIR"
-  printf "## ${!dep_name} ${!dep_version}\n" \
-    >> ${InstallDir}/${name}dependencies.sh
-  printf "source ${!dep_dir}/${!dep_name}env.sh\n" \
-    >> ${InstallDir}/${name}dependencies.sh
-done
+# general setup & environment
+source ../init.sh
 
-source ${InstallDir}/${name}dependencies.sh || exit 1
+# overwrite compiler for MPI
+export CC=${MPICXX}   # C compiler
+export CXX=${MPICXX} # C++ compiler
 
 ## download
 cd ${WORKING_DIR}
@@ -46,7 +33,8 @@ autoreconf -if
   --enable-openloops=${HEPSW_OPENLOOPS_DIR} --enable-root=${include_root} \
   --enable-rivet=${HEPSW_RIVET_DIR} --enable-recola=${HEPSW_RECOLA_DIR} \
   --enable-hepmc3=${HEPSW_HEPMC3_DIR} --enable-hepmc3root --enable-pythia \
-  --enable-mpi --enable-gzip CXXFLAGS="-std=c++11" --enable-ufo || exit 2
+  --enable-mpi --enable-gzip CXXFLAGS="-std=c++11" --enable-ufo \
+  --enable-lhole --enable-analysis || exit 2
 make -j${NUM_CORES} || exit 2
 make check || exit 3
 make install || exit 3
--- init.sh
+++ init.sh
@@ -2,7 +2,26 @@
 
 ## create basic folders
 mkdir -p ${HEPSW_HOME}
+mkdir -p ${WORKING_DIR}
+
+# copy README
 cp ../UserInfo/README.txt ${HEPSW_HOME}/README.txt
 sed  -i -e "s HEPSW_HOME ${HEPSW_HOME} g" ${HEPSW_HOME}/README.txt
 
-mkdir -p ${WORKING_DIR}
+## init dependencies
+mkdir -p ${InstallDir}
+printf "" > ${InstallDir}/${name}dependencies.sh
+for dep in ${dependencies[@]}; do
+  dep_name="HEPSW_${dep}_NAME"
+  dep_version="HEPSW_${dep}_VERSION"
+  dep_dir="HEPSW_${dep}_DIR"
+  printf "## ${!dep_name} ${!dep_version}\n" \
+    >> ${InstallDir}/${name}dependencies.sh
+  printf "source ${!dep_dir}/${!dep_name}env.sh\n" \
+    >> ${InstallDir}/${name}dependencies.sh
+  if [[ -e ${!dep_dir}/${!dep_name}dependencies.sh ]]; then
+    source ${!dep_dir}/${!dep_name}dependencies.sh
+  fi
+done
+
+source ${InstallDir}/${name}dependencies.sh || exit 1
