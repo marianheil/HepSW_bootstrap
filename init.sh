eca22c83c85de168ac4512b9441402514d7a0330
Marian Heil <marian.heil@durham.ac.uk>
2020-04-06 15:13:50 +0100

    Fix: Don't create dependencies folder for downloads (e.g. LHAPDF)

--- init.sh
+++ init.sh
@@ -9,19 +9,21 @@ cp ../UserInfo/README.txt ${HEPSW_HOME}/README.txt
 sed  -i -e "s HEPSW_HOME ${HEPSW_HOME} g" ${HEPSW_HOME}/README.txt
 
 ## init dependencies
-mkdir -p ${InstallDir}
-printf "" > ${InstallDir}/${name}dependencies.sh
-for dep in ${dependencies[@]}; do
-  dep_name="HEPSW_${dep}_NAME"
-  dep_version="HEPSW_${dep}_VERSION"
-  dep_dir="HEPSW_${dep}_DIR"
-  printf "## ${!dep_name} ${!dep_version}\n" \
-    >> ${InstallDir}/${name}dependencies.sh
-  printf "source ${!dep_dir}/${!dep_name}env.sh\n" \
-    >> ${InstallDir}/${name}dependencies.sh
-  if [[ -e ${!dep_dir}/${!dep_name}dependencies.sh ]]; then
-    source ${!dep_dir}/${!dep_name}dependencies.sh
-  fi
-done
+if [[ -n ${InstallDir} ]]; then
+  mkdir -p ${InstallDir}
+  printf "" > ${InstallDir}/${name}dependencies.sh
+  for dep in ${dependencies[@]}; do
+    dep_name="HEPSW_${dep}_NAME"
+    dep_version="HEPSW_${dep}_VERSION"
+    dep_dir="HEPSW_${dep}_DIR"
+    printf "## ${!dep_name} ${!dep_version}\n" \
+      >> ${InstallDir}/${name}dependencies.sh
+    printf "source ${!dep_dir}/${!dep_name}env.sh\n" \
+      >> ${InstallDir}/${name}dependencies.sh
+    if [[ -e ${!dep_dir}/${!dep_name}dependencies.sh ]]; then
+      source ${!dep_dir}/${!dep_name}dependencies.sh
+    fi
+  done
 
-source ${InstallDir}/${name}dependencies.sh
+  source ${InstallDir}/${name}dependencies.sh
+fi
