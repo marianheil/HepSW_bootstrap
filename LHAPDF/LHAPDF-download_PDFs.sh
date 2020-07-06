097d8a0e878bc4d3f6a944fa66eddf5ae09ba1c5
Marian Heil <marian.heil@durham.ac.uk>
2020-07-06 14:15:18 +0100

    Updated to LHAPDF 6.3.0

--- LHAPDF/LHAPDF-download_PDFs.sh
+++ LHAPDF/LHAPDF-download_PDFs.sh
@@ -4,27 +4,10 @@ source ../config
 source ../init.sh
 
 ## package specific variables
-InstallDir=${HEPSW_LHAPDF_DIR}
+source ${HEPSW_LHAPDF_DIR}/${HEPSW_LHAPDF_NAME}env.sh
 
 ## download all PDF sets
-all_pdfs=($(${InstallDir}/bin/lhapdf ls))
-installed_pdfs=($(${InstallDir}/bin/lhapdf ls --installed))
-failed_downloads=()
-for PDF in ${all_pdfs[@]}
-do
-  if (echo "${installed_pdfs[@]}"  | fgrep -wq "${PDF}")
-  then
-    echo "${PDF} already installed"
-  else
-    wget --no-check-certificate ${HEPSW_LHAPDF_PDF_SOURCE}/${PDF}.tar.gz -O- \
-      | tar xz -C ${InstallDir}/share/LHAPDF || failed_downloads+=(${PDF})
-  fi
-done
-
-if [ ${#failed_downloads[@]} -eq 0 ];
-then
-  echo "All PDF sets successfully downloaded."
-else
-  echo "Download failed for ${#failed_downloads[@]}/${#all_pdfs[@]} PDFs:"
-  echo ${failed_downloads[@]}
-fi
+all_pdfs=($(lhapdf ls))
+lhapdf update
+lhapdf get
+lhapdf upgrade
